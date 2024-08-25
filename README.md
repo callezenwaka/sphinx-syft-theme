c99a85c8d57149707cce6f898348f08123dcfd79
callezenwaka <callisezenwaka@gmail.com>
2024-08-25 17:49:21 +0100

    Fix lint issues

--- README.md
+++ README.md
@@ -19,21 +19,25 @@ twine upload dist/*
 This is Sphinx Syft Theme package. You can contribute via [Github](https://github.com/callezenwaka/sphinx-syft-theme/).
 
 Build files
+
 ```
 jupyter-book build .
 ```
 
 Build all files
+
 ```
 jupyter-book build --all .
 ```
 
 To delete the .jupyter_cache folder as well, add the --all flag like so
+
 ```
 jupyter-book clean . --all
 ```
 
 Install dependencies
+
 ```
 pip install -r requirements.txt
 
--- docs/conf.py
+++ docs/conf.py
@@ -1,69 +1,102 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
+"""Configuration file for the Sphinx documentation builder.
 
-import subprocess
+This file only contains a selection of the most common options. For a full
+list see the documentation:
+https://www.sphinx-doc.org/en/master/usage/configuration.html
+"""
+
+# -- Path setup --------------------------------------------------------------
+import os
 import sys
+from datetime import datetime
+from pathlib import Path
+from typing import Any, Dict
+
+from sphinx.application import Sphinx
+from sphinx.locale import _
+
+# from pkg_resources import get_distribution, DistributionNotFound
+# Import the package to get the version
+from sphinx_syft_theme import __version__
+
+# Get the current year
+current_year = datetime.now().year
+
+sys.path.append(str(Path(".").resolve()))
 
 # -- Project information -----------------------------------------------------
 
-author = "The OpenMined Community"
-copyright = "2023"
+project = "Syft Theme"
+copyright = f"{current_year}, Sphinx Syft Theme"
+author = "Syft Theme Maintainer"
 
 # -- General configuration ---------------------------------------------------
-
-exclude_patterns = ["**.ipynb_checkpoints", ".DS_Store", "Thumbs.db", "_build"]
-
-# -- General configuration ---------------------------------------------------
-
-pygments_style = "sphinx"
-
-suppress_warnings = ["myst.domains"]
-
-templates_path = ["_templates"]
-
-# -- Extensions configuration ------------------------------------------------
 
 extensions = [
+    "sphinx.ext.napoleon",
+    "sphinx.ext.autodoc",
+    # "sphinx.ext.autosummary",
+    "sphinx.ext.todo",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.graphviz",
+    "sphinxext.rediraffe",
+    "sphinx_design",
+    "sphinx_copybutton",
+    "autoapi.extension",
+    # custom extentions
+    # "_extension.gallery_directive",
+    # "_extension.component_directive",
+    # For extension examples and demos
+    "myst_parser",
     "ablog",
-    "myst_nb",
+    "jupyter_sphinx",
+    "sphinxcontrib.youtube",
+    "nbsphinx",
+    "numpydoc",
+    "sphinx_togglebutton",
+    "jupyterlite_sphinx",
+    "sphinx_favicon",
+    # "myst_nb",
     "sphinx_click.ext",
     "sphinx_comments",
-    "sphinx_copybutton",
     "sphinx_external_toc",
-    "sphinx_inline_tabs",
-    "sphinx_panels",
-    "sphinx_tabs.tabs",
+    # "sphinx_tabs.tabs",
     "sphinx_thebe",
-    "sphinx_togglebutton",
-    "sphinx.ext.autodoc",
-    "sphinx.ext.intersphinx",
-    "sphinx.ext.viewcode",
     "sphinxcontrib.bibtex",
     "sphinxext.opengraph",
 ]
 
-bibtex_bibfiles = ["references/references.bib"]
+jupyterlite_config = "jupyterlite_config.json"
 
-comments_config = {"hypothesis": False, "utterances": False}
+bibtex_bibfiles = ["references.bib"]
 
-external_toc_exclude_missing = False
-external_toc_path = "_toc.yml"
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ["_templates"]
 
-intersphinx_mapping = {
-    "python": ("https://docs.python.org/3.8", None),
-    "sphinx": ("https://www.sphinx-doc.org/en/master", None),
-}
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", "**.ipynb_checkpoints", ".tox"]
 
-jupyter_cache = ""
-jupyter_execute_notebooks = "cache"
-execution_allow_errors = False
-execution_excludepatterns = []
-execution_in_temp = False
-execution_timeout = 30
+intersphinx_mapping = {"sphinx": ("https://www.sphinx-doc.org/en/master", None)}
 
+external_toc_path = "_toc.yml"  # optional, default: _toc.yml
+external_toc_exclude_missing = False  # optional, default: False
+
+# -- Sitemap -----------------------------------------------------------------
+
+# ReadTheDocs has its own way of generating sitemaps, etc.
+if not os.environ.get("READTHEDOCS"):
+    extensions += ["sphinx_sitemap"]
+
+    html_baseurl = os.environ.get("SITEMAP_URL_BASE", "http://127.0.0.1:8000/")
+    sitemap_locales = [None]
+    sitemap_url_scheme = "{link}"
+
+# -- MyST options ------------------------------------------------------------
+
+# This allows us to use ::: to denote directives, useful for admonitions
 myst_enable_extensions = [
     "amsmath",
     "colon_fence",
@@ -76,111 +109,249 @@ myst_enable_extensions = [
     "smartquotes",
     "substitution",
 ]
-myst_substitutions = {"sub3": "My _global_ value!"}
+myst_heading_anchors = 2
+myst_substitutions = {"rtd": "[Read the Docs](https://readthedocs.org/)"}
 myst_url_schemes = ["mailto", "http", "https"]
 
-nb_output_stderr = "show"
+# -- Internationalization ----------------------------------------------------
 
-nitpick_ignore = [
-    ("py:class", "docutils.nodes.document"),
-    ("py:class", "docutils.parsers.rst.directives.body.Sidebar"),
-]
+# specifying the natural language populates some key tags
+language = "en"
 
-numfig = True
+# -- Ablog options -----------------------------------------------------------
 
-panels_add_bootstrap_css = False
-
-source_suffix = {
-    ".rst": "restructuredtext",
+blog_path = ""
+blog_authors = {
+    "jupyter": ("Jupyter", "https://jupyter.org"),
 }
 
-thebe_config = {
-    "repository_url": "https://github.com/binder-examples/jupyter-stacks-datascience",
-    "repository_branch": "master",
-}
 
-use_jupyterbook_latex = True
-use_multitoc_numbering = True
+# -- sphinx_ext_graphviz options ---------------------------------------------
+
+graphviz_output_format = "svg"
+inheritance_graph_attrs = dict(
+    rankdir="LR",
+    fontsize=14,
+    ratio="compress",
+)
+
+# -- sphinx_togglebutton options ---------------------------------------------
+togglebutton_hint = str(_("Click to expand"))
+togglebutton_hint_hide = str(_("Click to collapse"))
+
+# -- Sphinx-copybutton options ---------------------------------------------
+# Exclude copy button from appearing over notebook cell numbers by using :not()
+# The default copybutton selector is `div.highlight pre`
+# https://github.com/executablebooks/sphinx-copybutton/blob/master/sphinx_copybutton/__init__.py#L82
+copybutton_selector = ":not(.prompt) > div.highlight pre"
 
 # -- Options for HTML output -------------------------------------------------
 
 html_theme = "sphinx_syft_theme"
-html_logo = "images/dummy_logo_dark.svg"
-html_title = "Syft Theme"
-html_copy_source = True
+html_logo = "_static/images/logo-dark.svg"
+html_favicon = "_static/images/favicon.ico"
 html_sourcelink_suffix = ""
-html_favicon = "images/favicon.ico"
-html_last_updated_fmt = ""
+html_last_updated_fmt = ""  # to reveal the build date in the pages meta
 
-html_sidebars = {
-    "index": [],
-    "standalone": [],
-    "references/blog/*": [
-        "sidebar-logo.html",
-        "search-field.html",
-        "postcard.html",
-        "recentposts.html",
-        "tagcloud.html",
-        "categories.html",
-        "archives.html",
-        "sbt-sidebar-nav.html",
-        "sbt-sidebar-footer.html",
-    ],
-}
+# Define the json_url for our version switcher.
+# json_url = "https://sphinx-syft-theme.readthedocs.io/en/latest/_static/switcher.json"
 
-# html_static_path = ["_static"]
+# # Define the version we use for matching in the version switcher.
+# version_match = os.environ.get("READTHEDOCS_VERSION")
+# release = sphinx_syft_theme.__version__
+# # If READTHEDOCS_VERSION doesn't exist, we're not on RTD
+# # If it is an integer, we're in a PR build and the version isn't correct.
+# # If it's "latest" → change to "dev" (that's what we want the switcher to call it)
+# if not version_match or version_match.isdigit() or version_match == "latest":
+#     # For local development, infer the version to match from the package.
+#     if "dev" in release or "rc" in release:
+#         version_match = "dev"
+#         # We want to keep the relative reference if we are in dev mode
+#         # but we want the whole url if we are effectively in a released version
+#         json_url = "_static/switcher.json"
+#     else:
+#         version_match = f"v{release}"
+# elif version_match == "stable":
+#     version_match = f"v{release}"
 
+#
 html_theme_options = {
-    "github_url": "https://github.com/callezenwaka/syft-theme",
-    "twitter_url": "https://twitter.com/openmined",
-    "repository_url": "https://github.com/callezenwaka/syft-theme",
-    "launch_buttons": {
-        "binderhub_url": "https://mybinder.org",
-        "colab_url": "https://colab.research.google.com/",
-        "notebook_interface": "classic",
-        "thebe": True,
+    "analytics": {
+        "plausiable__analytics_domain": "foo",
+        "plausible__analytics_url": '<script defer="defer"> data-domain=toto src=http://.../script.js </script>,',
     },
-    "use_edit_page_button": True,
-    "use_issues_button": True,
-    "use_repository_button": True,
-    "use_download_button": True,
-    "logo_only": True,
-    "logo_link": "https://syftbook.readthedocs.io/",
-    "show_toc_level": 2,
-    "navbar_align": "left",
+    "header_links_before_dropdown": 4,
     "navbar_links": [
-        {"name": "Documentation", "url": "index#documentation"},
+        {"name": "Documentation", "url": "index"},
+        {"name": "About", "url": "about"},
+        {"name": "Contributing", "url": "contribute/setup"},
+        {"name": "Tutorial", "url": "tutorials/get-started"},
     ],
     "external_links": [
         {
-            "name": "GitHub",
-            "url": "https://github.com/callezenwaka/syft-theme",
+            "url": "https://sphinx-syft-theme.readthedocs.io/en/latest/",
+            "name": "Syft Theme",
         },
     ],
-    "page_layouts": {
-        "index": "page-banner.html",
-        "standalone": "page-standalone.html",
-    },
-    "footer_logos": {
-        "NCAR": {
-            "image": "images/NCAR-contemp-logo-blue.svg",
-            "url": "https://ncar.ucar.edu",
+    "icon_links": [
+        {
+            "name": "Twitter",
+            "url": "https://twitter.com/callezenwaka",
+            "icon": "_static/images/icon/twitter.svg",
+            "type": "local",
         },
-        "Unidata": {
-            "image": "images/Unidata_logo_horizontal_1200x300.svg",
+        {
+            "name": "GitHub",
+            "url": "https://github.com/callezenwaka/sphinx-syft-theme",
+            "icon": "_static/images/icon/github.svg",
+            "type": "local",
         },
-        "UAlbany": "images/UAlbany-A2-logo-purple-gold.svg",
+        {
+            "name": "Slack",
+            "url": "https://github.com/callezenwaka/sphinx-syft-theme",
+            "icon": "_static/images/icon/slack.svg",
+            "type": "local",
+        },
+    ],
+    "text_links": [
+        {
+            "name": "Join on slack for #support",
+            "url": "https://slack.com/",
+            "icon": "_static/images/icon/slack.svg",
+            "type": "local",
+        },
+    ],
+    "logo": {
+        "text": "Syft Theme",
+        "image_light": "_static/images/logo-light.svg",
     },
-    "extra_navbar": ('Theme by <a href="https://openmined.org">OpenMined Syft Project</a>'),
+    "use_edit_page_button": True,
+    "show_sidebar_on_root_doc": True,  # or False
+    "home_page_in_toc": True,
+    "show_toc_level": 2,
+    "navbar_align": "content",  # [left, content, right] For testing that the navbar items align properly
+    "show_nav_level": 2,
+    "announcement": f"⚠️This is an experimental release <a href='https://pypi.org/project/sphinx-syft-theme/{__version__}/' target='_blank'>sphinx-syft-theme</a> ⚠️",
+    "show_version_warning_banner": True,
+    # "navbar_center": ["version-switcher", "navbar-menu"],
+    # "navbar_start": ["navbar-logo"],
+    # "navbar_end": ["theme-switcher", "navbar-icon-links"],
+    # "navbar_persistent": ["search-button"],
+    # "primary_sidebar_end": ["custom-template", "sidebar-ethical-ads"],
+    # "article_footer_items": ["test", "test"],
+    # "content_footer_items": ["test", "test"],
+    # "footer_start": ["copyright"],
+    # "footer_center": ["sphinx-version"],
+    # "secondary_sidebar_items": ["page-toc", "edit-this-page", "sourcelink"],
+    "secondary_sidebar_items": {
+        "**": ["page-toc", "edit-this-page", "sourcelink"],
+        # "**/*": ["page-toc", "edit-this-page", "sourcelink"],
+        "standalone": [],
+    },
+    # "back_to_top_button": False,
+    "custom_shortcode_to_image": {
+        "|:persona:|": "/_static/images/persona.png",
+    },
+}
+# This ensures testing for custom sidebars
+html_sidebars = {
+    "**": [
+        "search-button-field.html",
+        "sidebar-nav-bs",
+    ],
+    "standalone": [],
 }
 
-blog_path = "references/blog"
-blog_post_pattern = "references/blog/*.md"
-blog_baseurl = "https://syftbook.readthedocs.io"
-fontawesome_included = True
-post_auto_image = 1
-post_auto_excerpt = 2
+html_context = {
+    "github_user": "callezenwaka",
+    "github_repo": "sphinx-syft-theme",
+    "github_version": "main",
+    "doc_path": "docs",
+}
 
-# ==============================================================================
+rediraffe_redirects = {
+    # "contributing.md": "contributing/setup.md",
+}
 
-subprocess.run([sys.executable, "getreferences.py", "./references"])
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ["_static"]
+html_css_files = ["css/custom.css"]
+html_js_files = ["js/syft-icon.js", "js/custom-icon.js"]
+todo_include_todos = True
+
+# -- favicon options ---------------------------------------------------------
+
+# see https://sphinx-favicon.readthedocs.io for more information about the
+# sphinx-favicon extension
+favicons = [
+    # generic icons compatible with most browsers
+    "images/favicon/favicon-32x32.png",
+    "images/favicon/favicon-16x16.png",
+    {"rel": "shortcut icon", "sizes": "any", "href": "favicon.ico"},
+    # chrome specific
+    "images/favicon/android-chrome-192x192.png",
+    # apple icons
+    {"rel": "apple-touch-icon", "href": "images/favicon/apple-touch-icon.png"},
+    # msapplications
+    {"name": "msapplication-TileColor", "content": "#459db9"},
+    {"name": "theme-color", "content": "#ffffff"},
+    {"name": "msapplication-TileImage", "content": "images/favicon/mstile-150x150.png"},
+]
+
+# -- Options for autosummary/autodoc output ------------------------------------
+# autosummary_generate = True
+autodoc_typehints = "description"
+autodoc_member_order = "groupwise"
+numpydoc_class_members_toctree = False
+
+# -- Options for autoapi -------------------------------------------------------
+autoapi_type = "python"
+autoapi_generate_api_docs = False
+autoapi_dirs = ["../src/sphinx_syft_theme"]
+autoapi_keep_files = False
+autoapi_root = "api"
+autoapi_member_order = "groupwise"
+autoapi_add_toctree_entry = False
+
+suppress_warnings = ["autosummary"]
+
+# -- application setup -------------------------------------------------------
+
+
+def setup_to_main(
+    app: Sphinx, pagename: str, templatename: str, context, doctree
+) -> None:
+    """Add a function that jinja can access for returning an "edit this page" link pointing to `main`."""
+
+    def to_main(link: str) -> str:
+        """Transform "edit on github" links and make sure they always point to the main branch.
+
+        Args:
+            link: the link to the github edit interface
+
+        Returns:
+            the link to the tip of the main branch for the same file
+        """
+        links = link.split("/")
+        idx = links.index("edit")
+        return "/".join(links[: idx + 1]) + "/main/" + "/".join(links[idx + 2 :])
+
+    context["to_main"] = to_main
+
+
+def setup(app: Sphinx) -> Dict[str, Any]:
+    """Add custom configuration to sphinx app.
+
+    Args:
+        app: the Sphinx application
+    Returns:
+        the 2 parallel parameters set to ``True``.
+    """
+    app.connect("html-page-context", setup_to_main)
+
+    return {
+        "parallel_read_safe": True,
+        "parallel_write_safe": True,
+    }
